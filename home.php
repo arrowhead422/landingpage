10d438132070d108a800806f554cd5fd522e7031
heroes618 <heroes618@hotmail.com>
2015-05-21 08:52:04 -0700

    landing part 1

---
+++ home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet"  href="css/main.css">
+</head>
+<body>
+<p id="title">Portfolio</p>
+<a href="http://www.google.com"><p class="guide">Contact &nbsp</p></a>
+<a href="http://www.google.com"><p class="guide">F.A.Q. &nbsp</p></a>
+<a href="http://www.google.com"><p class="guide">About &nbsp</p></a>
+<a href="http://codepen.io/zenturtle/full/aAcwH"><p class="guide">Home &nbsp</p></a>
+</br></br>
+<hr>
+</br></br></br>
+<table align="center";>
+  <tr>
+    <td>
+      <a href="http://codepen.io/zenturtle/full/oAbjC/"><img class="gallery" src="http://i1383.photobucket.com/albums/ah319/zen_turtle_online/Stock%20Photos/DSC_0236_zps5ff19afa.jpg"></img></a>
+    </td>
+    <td>
+      <img class="gallery" src="http://i1383.photobucket.com/albums/ah319/zen_turtle_online/Stock%20Photos/DSC_0200_zpsb1aa0e8d.jpg"></img>
+    </td>
+    <td>
+      <img class="gallery" src="http://i1383.photobucket.com/albums/ah319/zen_turtle_online/Stock%20Photos/DSC_0291_zps20c7793a.jpg"></img>
+    </td>
+<td>
+  <img class="gallery" src="http://i1383.photobucket.com/albums/ah319/zen_turtle_online/Stock%20Photos/file8261246814968_zpsffd2cca9.jpg"></img>
+</td>
+  </tr>
+<tr>
+  <td>
+    <img class="gallery" src="http://i1383.photobucket.com/albums/ah319/zen_turtle_online/Stock%20Photos/file1301234046357_zpsd640b212.jpg"></img>
+  </td>
+   <td>
+     <img class="gallery" src="http://i1383.photobucket.com/albums/ah319/zen_turtle_online/Stock%20Photos/file0001601969844_zps15693f8b.jpg"></img>
+  </td>
+   <td>
+     <img class="gallery" src="http://i1383.photobucket.com/albums/ah319/zen_turtle_online/Stock%20Photos/file000626266718_zps390bce1c.jpg"></img>
+  </td>
+   <td>
+     <img class="gallery" src="http://i1383.photobucket.com/albums/ah319/zen_turtle_online/Stock%20Photos/file451264266022_zps203b1468.jpg"></img>
+  </td>
+</tr>
+    <p id="birthdays">Birthdays</p>
+</table>
+
+</body>
+</html>
